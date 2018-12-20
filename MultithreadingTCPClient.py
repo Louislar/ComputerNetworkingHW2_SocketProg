17a5d2a391f300a362b06edd7e28bf143e53af82
Louislar <a8945267@gmail.com>
2018-12-20 23:08:55 +0800

    update20181220

--- MultithreadingTCPClient.py
+++ MultithreadingTCPClient.py
@@ -76,7 +76,8 @@ class MultithreadingTCPClient:
     #送一次訊息給server
     def sendToServer(self, sendMsg, myID):
         try:
-            sentence = sendMsg + ' ---by '+ myID + ' from ' + str(self.clientAddress)+ ':' + str(self.clientPort)
+            #sentence = sendMsg + ' ---by '+ myID + ' from ' + str(self.clientAddress)+ ':' + str(self.clientPort)
+            sentence = '[' + myID + ']: ' + sendMsg
             self.client_socket.send(sentence.encode())        #送出訊息給server
             self.isConnect = True
         except:
